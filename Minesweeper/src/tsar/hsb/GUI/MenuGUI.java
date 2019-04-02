e9641d891e2fd2eabad8e2ea80e96ea9ec72aed5
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2019-04-01 22:37:40 -0400

    Updated MenuGUI
    -Removed Options Button and Panel
    -Added MenuPanel
    -Added Back Button
    -Added Panel Swapping and Button Functionality

--- Minesweeper/src/tsar/hsb/GUI/MenuGUI.java
+++ Minesweeper/src/tsar/hsb/GUI/MenuGUI.java
@@ -1,6 +1,8 @@
 package tsar.hsb.GUI;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,7 +15,7 @@ public class MenuGUI extends JFrame {
 
 	private final int WIDTH = 400, HEIGHT = 500;
 
-	private JPanel startPanel, optionPanel;
+	private JPanel startPanel, menuPanel;
 
 	private CustomFont font;
 
@@ -26,25 +28,57 @@ public class MenuGUI extends JFrame {
 		this.getContentPane().setBackground(Color.DARK_GRAY);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		font = new CustomFont();
+
+		startPanel = new JPanel();
+		menuPanel = new JPanel();
 	}
 
 	public void start() {
-		this.add(addStartButton());
-		this.add(addExitButton());
-		this.add(addOptionsButton());
-		this.add(addLogo("Minesweeper"));
+		initializePanels();
+		swapToMenuPanel();
 		this.setVisible(true);
-		this.revalidate();
+	}
+
+	private void initializePanels() {
+		startPanel.setBounds(0, 0, this.WIDTH, this.HEIGHT);
+		startPanel.add(addLogo("Minesweeper"));
+		startPanel.setBackground(Color.DARK_GRAY);
+		startPanel.setLayout(null);
+		startPanel.setVisible(true);
+		startPanel.setEnabled(true);
+		startPanel.add(addBackButton());
+
+		menuPanel.setBounds(0, 0, this.WIDTH, this.HEIGHT);
+		menuPanel.setBackground(Color.DARK_GRAY);
+		menuPanel.setLayout(null);
+		menuPanel.setVisible(true);
+		menuPanel.setEnabled(true);
+		menuPanel.add(addExitButton());
+		menuPanel.add(addStartButton());
+		menuPanel.add(addLogo("Minesweeper"));
+	}
+
+	private void swapToStartPanel() {
+		this.remove(menuPanel);
+		this.add(startPanel);
 		this.repaint();
+		this.revalidate();
+	}
+
+	private void swapToMenuPanel() {
+		this.remove(startPanel);
+		this.add(menuPanel);
+		this.repaint();
+		this.revalidate();
 	}
 
 	private JLabel addLogo(String logoText) {
 		JLabel logoLabel = new JLabel(logoText);
-		logoLabel.setBounds(0, 10, this.WIDTH, 70);
+		logoLabel.setBounds(0, 30, this.WIDTH, 80);
 		logoLabel.setForeground(Color.CYAN);
 		logoLabel.setHorizontalAlignment(JLabel.CENTER);
 		logoLabel.setVerticalAlignment(JLabel.CENTER);
-		logoLabel.setFont(this.font.getFont(60f));
+		logoLabel.setFont(this.font.getFont(65f));
 		return logoLabel;
 	}
 
@@ -56,9 +90,16 @@ public class MenuGUI extends JFrame {
 		startButton.setHorizontalAlignment(JButton.CENTER);
 		startButton.setEnabled(true);
 		startButton.setFont(this.font.getFont(40f));
-		startButton.setBounds((this.WIDTH / 3), 150, this.WIDTH / 3, 40);
+		startButton.setBounds((this.WIDTH / 3), 170, this.WIDTH / 3, 40);
 		startButton.setFocusPainted(false);
 		startButton.setBorderPainted(false);
+		startButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				swapToStartPanel();
+			}
+		});
 		return startButton;
 	}
 
@@ -70,24 +111,38 @@ public class MenuGUI extends JFrame {
 		exitButton.setHorizontalAlignment(JButton.CENTER);
 		exitButton.setEnabled(true);
 		exitButton.setFont(this.font.getFont(40f));
-		exitButton.setBounds((this.WIDTH / 3), 290, this.WIDTH / 3, 40);
+		exitButton.setBounds((this.WIDTH / 3), 250, this.WIDTH / 3, 40);
 		exitButton.setFocusPainted(false);
 		exitButton.setBorderPainted(false);
+		exitButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		return exitButton;
 	}
 
-	private JButton addOptionsButton() {
-		JButton optionsButton = new JButton("Options");
-		optionsButton.setForeground(Color.CYAN);
-		optionsButton.setBackground(Color.DARK_GRAY);
-		optionsButton.setVerticalAlignment(JButton.CENTER);
-		optionsButton.setHorizontalAlignment(JButton.CENTER);
-		optionsButton.setEnabled(true);
-		optionsButton.setFont(this.font.getFont(40f));
-		optionsButton.setBounds((this.WIDTH / 4), 220, this.WIDTH / 2, 40);
-		optionsButton.setFocusPainted(false);
-		optionsButton.setBorderPainted(false);
-		return optionsButton;
+	private JButton addBackButton() {
+		JButton backButton = new JButton("Back");
+		backButton.setForeground(Color.CYAN);
+		backButton.setBackground(Color.DARK_GRAY);
+		backButton.setVerticalAlignment(JButton.CENTER);
+		backButton.setHorizontalAlignment(JButton.CENTER);
+		backButton.setEnabled(true);
+		backButton.setFont(this.font.getFont(40f));
+		backButton.setBounds((this.WIDTH / 3), 355, this.WIDTH / 3, 40);
+		backButton.setFocusPainted(false);
+		backButton.setBorderPainted(false);
+		backButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				swapToMenuPanel();
+			}
+		});
+		return backButton;
 	}
 
 }
