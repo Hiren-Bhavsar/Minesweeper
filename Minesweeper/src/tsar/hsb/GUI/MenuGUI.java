ac0db8bae1783ee51ac2e9eb3df929552e89dbee
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2019-04-01 23:02:03 -0400

    MenuGUI Update
    -Added gameButtons
    -Added Float Parameter to buttonInitializer
    -TODO Finish Start Game Options Action Listeners

--- Minesweeper/src/tsar/hsb/GUI/MenuGUI.java
+++ Minesweeper/src/tsar/hsb/GUI/MenuGUI.java
@@ -23,12 +23,11 @@ public class MenuGUI extends JFrame {
 		this.setSize(this.WIDTH, this.HEIGHT);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
-		this.setTitle("Menu GUI");
+		this.setTitle("Minesweeper By Tsar");
 		this.setLayout(null);
 		this.getContentPane().setBackground(Color.DARK_GRAY);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		font = new CustomFont();
-
 		startPanel = new JPanel();
 		menuPanel = new JPanel();
 	}
@@ -46,6 +45,9 @@ public class MenuGUI extends JFrame {
 		startPanel.setLayout(null);
 		startPanel.setVisible(true);
 		startPanel.add(addBackButton());
+		for (JButton b : addGameButtons()) {
+			startPanel.add(b);
+		}
 
 		menuPanel.setBounds(0, 0, this.WIDTH, this.HEIGHT);
 		menuPanel.setBackground(Color.DARK_GRAY);
@@ -82,7 +84,7 @@ public class MenuGUI extends JFrame {
 
 	private JButton addStartButton() {
 		JButton startButton = new JButton("Start");
-		buttonInitializer(startButton);
+		buttonInitializer(startButton, 40f);
 		startButton.setBounds((this.WIDTH / 3), 170, this.WIDTH / 3, 40);
 		startButton.addActionListener(new ActionListener() {
 
@@ -96,7 +98,7 @@ public class MenuGUI extends JFrame {
 
 	private JButton addExitButton() {
 		JButton exitButton = new JButton("Exit");
-		buttonInitializer(exitButton);
+		buttonInitializer(exitButton, 40f);
 		exitButton.setBounds((this.WIDTH / 3), 250, this.WIDTH / 3, 40);
 		exitButton.addActionListener(new ActionListener() {
 
@@ -110,8 +112,7 @@ public class MenuGUI extends JFrame {
 
 	private JButton addBackButton() {
 		JButton backButton = new JButton("Back");
-		buttonInitializer(backButton);
-		backButton.setFont(this.font.getFont(40f));
+		buttonInitializer(backButton, 40f);
 		backButton.setBounds((this.WIDTH / 3), 355, this.WIDTH / 3, 40);
 		backButton.addActionListener(new ActionListener() {
 
@@ -123,13 +124,60 @@ public class MenuGUI extends JFrame {
 		return backButton;
 	}
 
-	private void buttonInitializer(JButton b) {
+	private JButton[] addGameButtons() {
+		JButton gameButtons[] = new JButton[3];
+
+		for (int x = 0; x < 3; x++) {
+			gameButtons[x] = new JButton();
+			buttonInitializer(gameButtons[x], 32f);
+			gameButtons[x].addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JButton temp = (JButton) e.getSource();
+
+					switch (temp.getText()) {
+
+					case "Easy":
+						// TODO
+						break;
+
+					case "Medium":
+						// TODO
+						break;
+
+					case "Hard":
+						// TODO
+						break;
+
+					default:
+						break;
+					}
+				}
+			});
+		}
+
+		gameButtons[0].setText("Easy");
+		gameButtons[1].setText("Medium");
+		gameButtons[2].setText("Hard");
+
+		gameButtons[0].setBounds((this.WIDTH / 3), 160, this.WIDTH / 3, 40);
+		gameButtons[0].setForeground(Color.GREEN);
+		gameButtons[1].setBounds((this.WIDTH / 3), 220, this.WIDTH / 3, 40);
+		gameButtons[1].setForeground(Color.ORANGE);
+		gameButtons[2].setBounds((this.WIDTH / 3), 280, this.WIDTH / 3, 40);
+		gameButtons[2].setForeground(Color.RED);
+
+		return gameButtons;
+	}
+
+	private void buttonInitializer(JButton b, float fSize) {
 		b.setForeground(Color.CYAN);
 		b.setBackground(Color.DARK_GRAY);
 		b.setVerticalAlignment(JButton.CENTER);
 		b.setHorizontalAlignment(JButton.CENTER);
 		b.setEnabled(true);
-		b.setFont(this.font.getFont(40f));
+		b.setFont(this.font.getFont(fSize));
 		b.setFocusPainted(false);
 		b.setBorderPainted(false);
 	}
