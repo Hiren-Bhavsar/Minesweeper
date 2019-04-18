39742a69943d03519fa94e92380cd74bf7bd0184
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2019-04-18 08:32:17 -0400

    Controller Initialization Update
    -Added Initialization of Quadrates
    -Added Basic Random Mine Generation (Will make this better later).
    -Added print method for gameBoard for debugging purposes.

--- Minesweeper/src/tsar/hsb/Controller.java
+++ Minesweeper/src/tsar/hsb/Controller.java
@@ -3,6 +3,7 @@ package tsar.hsb;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,11 +12,22 @@ import tsar.hsb.GUI.GameFrame;
 public class Controller {
 
 	private int width, height, numberOfMines;
+
 	private GameFrame gFrame;
+
+	private Quadrate[][] gameBoard;
 
 	public Controller(String difficulty) {
 		initializeGameData(difficulty.toLowerCase());
+
 		gFrame = new GameFrame(this.width, this.height);
+
+		gameBoard = new Quadrate[this.width][this.height];
+
+		initializeQuadrates();
+
+		printGameBoard();
+
 	}
 
 	private void initializeGameData(String difficulty) {
@@ -31,9 +43,44 @@ public class Controller {
 		this.width = Integer.parseInt(gameData[1]);
 		this.height = Integer.parseInt(gameData[2]);
 		this.numberOfMines = Integer.parseInt(gameData[3]);
+	}
 
-		System.out.println(this.width + "  " + this.height + "  " + this.numberOfMines);
+	private void initializeQuadrates() {
+		for (int x = 0; x < this.width; x++) {
+			for (int y = 0; y < this.height; y++) {
+				gameBoard[x][y] = new Quadrate(x, y);
+			}
+		}
+		addMines();
+	}
 
+	private void addMines() {
+		//TODO Allow a mine to be added only if 2 or less neighbors are also mines. Prevent Grouping
+		Random r = new Random();
+		int temp = this.numberOfMines;
+		while (temp != 0) {
+			int x = r.nextInt(this.width);
+			int y = r.nextInt(this.height);
+
+			if (!gameBoard[x][y].isMine() && (x != 0 && y != 0)) {
+				temp--;
+				gameBoard[x][y].setIsMine(true);
+			}
+		}
+	}
+
+	private void printGameBoard() {
+		System.out.println();
+		for (int y = 0; y < this.height; y++) {
+			for (int x = 0; x < this.width; x++) {
+				if (gameBoard[x][y].isMine()) {
+					System.out.print("M ");
+				} else {
+					System.out.print(gameBoard[x][y].getNumberValue() + " ");
+				}
+			}
+			System.out.println("");
+		}
 	}
 
 }
