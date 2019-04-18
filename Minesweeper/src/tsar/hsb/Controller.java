4f659952e8b24790d7e141a1f99b6897819ff7d9
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2019-04-18 08:44:50 -0400

    Controller Update
    -Added method to count the number of neighbours that are mines.
    
    -Set the correct number for each Quadrate.
    
    -Implemented anti grouping in the addMine method.

--- Minesweeper/src/tsar/hsb/Controller.java
+++ Minesweeper/src/tsar/hsb/Controller.java
@@ -52,21 +52,46 @@ public class Controller {
 			}
 		}
 		addMines();
+
+		for (int x = 0; x < this.width; x++) {
+			for (int y = 0; y < this.height; y++) {
+				gameBoard[x][y].setNumberValue(countMineNeighbours(x, y));
+			}
+		}
 	}
 
 	private void addMines() {
-		//TODO Allow a mine to be added only if 2 or less neighbors are also mines. Prevent Grouping
 		Random r = new Random();
 		int temp = this.numberOfMines;
 		while (temp != 0) {
 			int x = r.nextInt(this.width);
 			int y = r.nextInt(this.height);
 
-			if (!gameBoard[x][y].isMine() && (x != 0 && y != 0)) {
+			if (!gameBoard[x][y].isMine() && (x != 0 && y != 0) && countMineNeighbours(x, y) < 3) {
 				temp--;
 				gameBoard[x][y].setIsMine(true);
 			}
 		}
+	}
+
+	private int countMineNeighbours(int x, int y) {
+		int neighbourCount = 0;
+
+		for (int a = -1; a < 2; a++) {
+			for (int b = -1; b < 2; b++) {
+				if (a == 0 && b == 0) {
+
+				} else {
+					if (((a + x) >= 0 && (a + x) < width) && ((b + y) >= 0 && (b + y) < height)) {
+						if (gameBoard[a + x][b + y].isMine()) {
+							neighbourCount++;
+						}
+					}
+				}
+			}
+		}
+
+		return neighbourCount;
 	}
 
 	private void printGameBoard() {
