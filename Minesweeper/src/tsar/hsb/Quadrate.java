7ebe1988b2f080ed28944e930bc05756478f544d
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2019-04-26 14:15:16 -0400

    Added isRevealed boolean to Quadrate

--- Minesweeper/src/tsar/hsb/Quadrate.java
+++ Minesweeper/src/tsar/hsb/Quadrate.java
@@ -4,12 +4,13 @@ public class Quadrate {
 
 	private int x, y, numberValue;
 
-	private boolean isMine;
+	private boolean isMine, isRevealed;
 
 	public Quadrate(int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.isMine = false;
+		this.isRevealed = false;
 		this.setNumberValue(0);
 	}
 
@@ -35,6 +36,14 @@ public class Quadrate {
 
 	public boolean isMine() {
 		return this.isMine;
+	}
+
+	public void setIsRevealed(boolean isRevealed) {
+		this.isRevealed = isRevealed;
+	}
+
+	public boolean isRevealed() {
+		return this.isRevealed;
 	}
 
 	public void setNumberValue(int numberValue) {
